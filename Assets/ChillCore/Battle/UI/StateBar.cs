eed9423c8ddf6e7650e8993373bf90611b376214
Castiel Faraday <2580911814@qq.com>
2026-04-13 00:21:27 +0800

    导入一批UI资源

--- Assets/ChillCore/Battle/UI/StateBar.cs
+++ Assets/ChillCore/Battle/UI/StateBar.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class StateBar : MonoBehaviour
 {
+    public GameObject healthBar;
+
+    public GameObject energyBar;
+
     // Start is called before the first frame update
     void Start()
     {
