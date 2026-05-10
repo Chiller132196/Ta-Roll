531e0602681708a932f48e96c726a978c0546d85
Castiel Faraday <2580911814@qq.com>
2026-05-10 21:48:34 +0800

    备战页面占坑

--- Assets/ChillCore/System/CommonStruct.cs
+++ Assets/ChillCore/System/CommonStruct.cs
@@ -16,3 +16,59 @@ public enum Chesstype
     Enemy,
     Neutral
 }
+
+/// <summary>
+/// 棋子的元素
+/// </summary>
+public enum ChessElement
+{
+    /// <summary>
+    /// 风元素
+    /// </summary>
+    Wind,
+    /// <summary>
+    /// 水元素
+    /// </summary>
+    Water,
+    /// <summary>
+    /// 火元素
+    /// </summary>
+    Fire,
+    /// <summary>
+    /// 土元素
+    /// </summary>
+    Soil
+}
+
+/// <summary>
+/// 棋子的数字阶层
+/// </summary>
+public enum ChessClass
+{
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    /// <summary>
+    /// 王后
+    /// </summary>
+    Queen,
+    /// <summary>
+    /// 骑士
+    /// </summary>
+    Knight,
+    /// <summary>
+    /// 国外
+    /// </summary>
+    King,
+    /// <summary>
+    /// 侍从
+    /// </summary>
+    Page,
+}
